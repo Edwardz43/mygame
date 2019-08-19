7e68fed5cddeb2712cbb3fa3db87879d448ce154
EdLo <b93303015@gmail.com>
2019-08-20 07:32:52 +0800

    Add new properties to user model.

--- gameserver/db/models/user.go
+++ gameserver/db/models/user.go
@@ -2,6 +2,8 @@ package models
 
 // User represent the users model
 type User struct {
-	ID   int64  `json:"id"`
-	Name string `json:"name"`
+	ID        int64  `json:"id"`
+	Name      string `json:"name"`
+	CreatedAt string `json:"created_at"`
+	UpdatedAt string `json:"updated_at"`
 }
